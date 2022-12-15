ffa5294b8e91685e2bcf1a974edfcbe3bd3cc950
TomasRampas <rampas.tomas@gmail.com>
2022-12-15 08:39:34 +0100

    Create 001_CodeNotes.cs

---
+++ 001_CodeNotes.cs
@@ -0,0 +1,9 @@
+    
+    // CODINIG RULES
+    // ------------------------------------------------------
+    // - bool should start with is -> isHungry
+    // - use camelback spacing -> moveSpeed
+    // - variables must have lower case names -> moveSpeed
+    // - functions/methods start with upper case names -> Start, Update
+    // - always use tabs for indentation never spaces
+    // - never put a semi column behind a brace -> };
