43111535d9dde0f944389a359c15c29a94f8f94c
TomasRampas <rampas.tomas@gmail.com>
2022-12-15 09:45:50 +0100

    Update 001_CodeNotes.cs

--- 001_CodeNotes.cs
+++ 001_CodeNotes.cs
@@ -1,9 +1,15 @@
-    
-    // CODINIG RULES
-    // ------------------------------------------------------
-    // - bool should start with is -> isHungry
-    // - use camelback spacing -> moveSpeed
-    // - variables must have lower case names -> moveSpeed
-    // - functions/methods start with upper case names -> Start, Update
-    // - always use tabs for indentation never spaces
-    // - never put a semi column behind a brace -> };
+
+// CODINIG RULES
+// ------------------------------------------------------
+// - bool should start with is -> isHungry
+// - use camelback spacing -> moveSpeed
+// - variables must have lower case names -> moveSpeed
+// - functions/methods start with upper case names -> Start, Update
+// - always use tabs for indentation never spaces
+// - never put a semi column behind a brace -> };
+
+
+// VOCABULARY
+// ------------------------------------------------------
+// - Instantiate = create instance of an object
+// - Instance = made out of prefab, there can be unlimited number of instances of an object
