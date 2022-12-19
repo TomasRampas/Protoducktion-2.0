5aa332ae3073230207595d263c323b52eb81bed1
TomasRampas <rampas.tomas@gmail.com>
2022-12-19 23:29:26 +0100

    Create 002_UI_Toolkit.cs

---
+++ 002_UI_Toolkit.cs
@@ -0,0 +1,19 @@
+// TERMINOLOGY
+// --------------------------------------------------------------
+// * Main/Corss-Axis
+// * Flex-Direction
+// * Aligning
+// * Flex-Basis
+// * Shrink/Grow
+// * Combinations
+
+// FLEXBOX TERMINOLOGY
+// --------------------------------------------------------------
+// * Container - contains other elements inside, there can be container in container
+// * Item - an end element, like item in an inventory
+// * Main / Cross Axis - they can be interchanged, it is not like x axis is always horizontal
+// the Flex Direction decides which is which, each axis has different rules and designer
+// needs to choose based on the usecase
+// * Flex Direction - only two states (COLUMN / ROW), is set on every container,
+// states flip the axis, ROW state, COLUMN state, if COLUMN
+// state used the Main Axis is on the Y Axis. If ROW tate used, the Main Axis is on the X Axis
